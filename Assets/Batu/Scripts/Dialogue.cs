3631fad75842ccad80d2271703298fde12491e33
Metin Batuhan Özücan <metbatuo@gmail.com>
2023-07-22 22:36:20 +0300

    NPC talk & anim finished

---
+++ Assets/Batu/Scripts/Dialogue.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+
+    private int index;
+    private Coroutine typingCoroutine;
+
+    private bool dialogueInProgress;
+    
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        dialogueInProgress = false;
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopCoroutine(typingCoroutine); // Stop the current typing coroutine
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    public void StartDialogue()
+    {
+        if (!dialogueInProgress)
+        {
+            textComponent.text = string.Empty;
+            gameObject.SetActive(true); // Ensure the canvas is active
+            dialogueInProgress = true;
+            typingCoroutine = StartCoroutine(TypeLine());
+        }
+        
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            typingCoroutine = StartCoroutine(TypeLine());
+        }
+        else
+        {
+            dialogueInProgress = false;
+            gameObject.SetActive(false); // Disable the canvas when dialogue ends
+        }
+    }
+    public void ResetDialogue()
+    {
+        dialogueInProgress = false;
+        index = 0;
+        textComponent.text = string.Empty;
+        gameObject.SetActive(false);
+    }
+}
