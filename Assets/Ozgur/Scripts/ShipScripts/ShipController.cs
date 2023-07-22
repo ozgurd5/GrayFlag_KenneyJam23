f8d37e0f4f63f0cf89b7724793bcdac8ff3412ef
Özgür Dalbeler <ozgurdalbeler@gmail.com>
2023-07-22 13:16:29 +0300

    ship accceleration done

--- Assets/Ozgur/Scripts/ShipScripts/ShipController.cs
+++ Assets/Ozgur/Scripts/ShipScripts/ShipController.cs
@@ -1,12 +1,21 @@
 using Cinemachine;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
     [Header("Assign")]
-    [SerializeField] private float normalSailSpeed = 3f;
-    [SerializeField] private float fullSailSpeed = 10f;
-
+    [SerializeField] private float maxSpeed = 0.5f;
+    [SerializeField] private float reverseSpeed = 0.1f;
+    [SerializeField] private float acceleration = 0.2f;
+    [SerializeField] private float deceleration = 0.5f;
+    [SerializeField] private float minRotationSpeed = 0.2f;
+    [SerializeField] private float maxRotationSpeed = 0.5f;
+    
+    [Header("Info - No touch")]
+    [SerializeField] private float movingSpeed;
+    [SerializeField] private float rotationSpeed;
+    
     private bool isShipControlled;
     
     private ShipInputManager sim;
@@ -14,14 +23,10 @@ public class ShipController : MonoBehaviour
     private Transform cameraLookAtTransform;
     private CinemachineVirtualCamera shipCamera;
 
-    private Vector3 movingDirection;
-    private float movingSpeed;
-
     private void Awake()
     {
-        //Moving speed default must be walking speed
-        movingSpeed = normalSailSpeed;
-
+        rotationSpeed = minRotationSpeed;
+        
         sim = GetComponent<ShipInputManager>();
         cameraFollowTransform = transform.Find("ShipCameraFollow");
         cameraLookAtTransform = transform.Find("ShipCameraLookAt");
@@ -36,10 +41,48 @@ public class ShipController : MonoBehaviour
 
     private void HandleMovement()
     {
-        transform.Translate(Vector3.forward * (sim.moveInput.y * movingSpeed));
-        transform.Rotate(0f, sim.moveInput.x * movingSpeed, 0f);
+        //Acceleration
+        if (sim.moveInput.y != 0f)
+        {
+            if (sim.moveInput.y > 0f)
+            {
+                movingSpeed += acceleration * Time.deltaTime;
+                movingSpeed = math.min(movingSpeed, maxSpeed);
+            }
+            
+            else
+            {
+                movingSpeed -= acceleration * Time.deltaTime;
+                movingSpeed = math.max(movingSpeed, -1 * reverseSpeed);
+            }
+            
+            rotationSpeed -= acceleration * Time.deltaTime;
+            rotationSpeed = math.max(rotationSpeed, minRotationSpeed);
+        }
+
+        //Deceleration
+        else
+        {
+            if (movingSpeed < 0f)
+            {
+                movingSpeed += deceleration * Time.deltaTime;
+                movingSpeed = math.min(movingSpeed, 0);
+            }
+
+            else
+            {
+                movingSpeed -= deceleration * Time.deltaTime;
+                movingSpeed = math.max(movingSpeed, 0);
+            }
+            
+            rotationSpeed += deceleration * Time.deltaTime;
+            rotationSpeed = math.min(rotationSpeed, maxRotationSpeed);
+        }
+
+        transform.Translate(Vector3.forward * movingSpeed);
+        transform.Rotate(0f, sim.moveInput.x * rotationSpeed, 0f);
     }
-    
+
     private void Update()
     {
         if (isShipControlled) HandleLooking();
