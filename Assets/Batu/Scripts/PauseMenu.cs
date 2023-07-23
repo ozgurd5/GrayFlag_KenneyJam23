593a491be71363c3737057a36b0a219113a64935
Metin Batuhan Özücan <metbatuo@gmail.com>
2023-07-23 08:20:28 +0300

    UI menu added

---
+++ Assets/Batu/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool Paused = false;
+    public GameObject PauseMenuCanvas;
+
+    void Start()
+    {
+        Time.timeScale = 1f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Paused)
+            {
+                Play();
+            }
+            else
+            {
+                Stop();
+            }
+        }
+    }
+
+    void Stop()
+    {
+        PauseMenuCanvas.SetActive(true);
+        Time.timeScale = 0f;
+        Paused = true; 
+        Debug.Log(" GAME PAUSED");
+
+    }
+
+    public void Play()
+    {
+        PauseMenuCanvas.SetActive(false);
+        Time.timeScale = 1f;
+        Paused = false;
+    }
+
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
