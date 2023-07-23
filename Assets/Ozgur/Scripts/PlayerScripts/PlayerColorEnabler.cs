f369810324d7da0732a1991c54ba778e53867c30
Özgür Dalbeler <ozgurdalbeler@gmail.com>
2023-07-23 10:36:53 +0300

    ship sword and gun colored

--- Assets/Ozgur/Scripts/PlayerScripts/PlayerColorEnabler.cs
+++ Assets/Ozgur/Scripts/PlayerScripts/PlayerColorEnabler.cs
@@ -6,11 +6,13 @@ public class PlayerColorEnabler : MonoBehaviour
     public static event Action OnRedColorEnabled;
     public static event Action OnGreenColorEnabled;
     public static event Action OnBlueColorEnabled;
+    public static event Action OnYellowColorEnabled;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.J)) OnRedColorEnabled?.Invoke();
         else if (Input.GetKeyDown(KeyCode.K)) OnGreenColorEnabled?.Invoke();
         else if (Input.GetKeyDown(KeyCode.L)) OnBlueColorEnabled?.Invoke();
+        else if (Input.GetKeyDown(KeyCode.H)) OnYellowColorEnabled?.Invoke();
     }
 }
