136ae6b7c89ea56b55bde7d083a75edf2a3cc222
Özgür Dalbeler <ozgurdalbeler@gmail.com>
2023-07-23 13:27:08 +0300

    runes are done

--- Assets/Ozgur/Scripts/PlayerScripts/PlayerColorEnabler.cs
+++ Assets/Ozgur/Scripts/PlayerScripts/PlayerColorEnabler.cs
@@ -15,4 +15,24 @@ public class PlayerColorEnabler : MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.L)) OnBlueColorEnabled?.Invoke();
         else if (Input.GetKeyDown(KeyCode.H)) OnYellowColorEnabled?.Invoke();
     }
+
+    public static void EnableRedColor()
+    {
+        OnRedColorEnabled?.Invoke();
+    }
+
+    public static void EnableGreenColor()
+    {
+        OnGreenColorEnabled?.Invoke();
+    }
+
+    public static void EnableBlueColor()
+    {
+        OnBlueColorEnabled?.Invoke();
+    }
+
+    public static void EnableYellowColor()
+    {
+        OnYellowColorEnabled?.Invoke();
+    }
 }
