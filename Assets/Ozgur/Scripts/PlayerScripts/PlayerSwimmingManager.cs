3af01b159e977dc0b32334e0632c2a4502bd17e2
Özgür Dalbeler <ozgurdalbeler@gmail.com>
2023-08-04 06:08:04 +0300

    weapon hide + splash sound

--- Assets/Ozgur/Scripts/PlayerScripts/PlayerSwimmingManager.cs
+++ Assets/Ozgur/Scripts/PlayerScripts/PlayerSwimmingManager.cs
@@ -3,22 +3,37 @@ using UnityEngine;
 public class PlayerSwimmingManager : MonoBehaviour
 {
     [Header("Assign")]
+    [SerializeField] private AudioSource splashSource;
     [SerializeField] private AudioSource normalSwimSource;
     [SerializeField] private AudioSource fastSwimSource;
-    
+    [SerializeField] private  float splashVelocityLimit = -30;
+
     private PlayerStateData psd;
+    private Rigidbody rb;
     
+    public bool splashCondition;
+
     private void Awake()
     {
         psd = GetComponent<PlayerStateData>();
+        rb = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
-        if (psd.isMoving && psd.isSwimming) PlaySound();
+        if (splashCondition && psd.isSwimming) PlaySplashSound();
+        if (psd.isMoving && psd.isSwimming) PlaySwimmingSound();
+
+        if (!splashCondition) splashCondition = rb.velocity.y < splashVelocityLimit;
     }
 
-    private void PlaySound()
+    private void PlaySplashSound()
+    {
+        splashSource.Play();
+        splashCondition = false;
+    }
+
+    private void PlaySwimmingSound()
     {
         if (normalSwimSource.isPlaying || fastSwimSource.isPlaying) return;
         
