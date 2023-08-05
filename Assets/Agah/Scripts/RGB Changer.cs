b42788fc6c93d4c9d195df27bccd20ef46ba2d45
Agah <agahgames260@gmail.com>
2023-08-05 21:57:01 +0300

    Renklerde Değişiklik, Kurukafa Sc'sinde düzenleme, adalarda düzenlemeler

--- Assets/Agah/Scripts/RGB Changer.cs
+++ Assets/Agah/Scripts/RGB Changer.cs
@@ -13,6 +13,42 @@ public class RGBChanger : MonoBehaviour
 
     float t = 0f;
 
+
+    bool redColorEnabled;
+    bool greenColorEnabled;
+    bool blueColorEnabled;
+    bool yellowColorEnabled;
+
+
+    private void Awake()
+    {
+        PlayerColorEnabler.OnBlueColorEnabled += PlayerColorEnabler_OnBlueColorEnabled;
+        PlayerColorEnabler.OnRedColorEnabled += PlayerColorEnabler_OnRedColorEnabled;
+        PlayerColorEnabler.OnGreenColorEnabled += PlayerColorEnabler_OnGreenColorEnabled;
+        PlayerColorEnabler.OnYellowColorEnabled += PlayerColorEnabler_OnYellowColorEnabled;
+    }
+
+    #region ReceiveEvents
+    private void PlayerColorEnabler_OnYellowColorEnabled()
+    {
+        yellowColorEnabled = true;
+    }
+
+    private void PlayerColorEnabler_OnGreenColorEnabled()
+    {
+        greenColorEnabled = true;
+    }
+
+    private void PlayerColorEnabler_OnRedColorEnabled()
+    {
+        redColorEnabled = true;
+    }
+
+    private void PlayerColorEnabler_OnBlueColorEnabled()
+    {
+        blueColorEnabled = true;
+    }
+    #endregion
     void Start()
     {
         cubeMeshRenderer = GetComponent<MeshRenderer>();
@@ -20,15 +56,27 @@ public class RGBChanger : MonoBehaviour
 
     void Update()
     {
+        if (IsAllColorsEnabled())
+            ChangeColors();
+    }
+
+    void ChangeColors()
+    {
         cubeMeshRenderer.material.color = Color.Lerp(cubeMeshRenderer.material.color, myColors[colorIndex], lerpTime * Time.deltaTime);
 
         t = Mathf.Lerp(t, 1f, lerpTime * Time.deltaTime);
-        if (t > .9f)
+        if (t > .99f)
         {
             t = 0f;
             colorIndex++;
             colorIndex = (colorIndex >= myColors.Length) ? 0 : colorIndex;
         }
+    }
+
+    bool IsAllColorsEnabled()
+    {
+        if(redColorEnabled && blueColorEnabled && greenColorEnabled && yellowColorEnabled) return true;
+        else return false;
     }
 }
 
