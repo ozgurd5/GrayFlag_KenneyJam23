e5ee6d2cd71434221fe1133e14137050c2d33250
Agah <agahgames260@gmail.com>
2023-08-17 13:13:54 +0300

    cutscene hazır

--- Assets/Agah/Scripts/Managers/FinalCutSceneManager.cs
+++ Assets/Agah/Scripts/Managers/FinalCutSceneManager.cs
@@ -7,6 +7,9 @@ public class FinalCutSceneManager : MonoBehaviour
 {
     [Tooltip("The change rate of the NORMALIZED position of the cameras.")]
     [SerializeField] float cameraMoveSpeed = 0.15f;
+    [Tooltip("The max NORMALIZED lenght of the dolly path camera can go before switching to the next camera." +
+        " The result of cameraMoveLimit / cameraMoveSpeed gives the time one camera takes to rotate around one island")]
+    [SerializeField] float cameraMoveLimit = 1.5f;
 
     [SerializeField]CameraManager cameraManager;
 
@@ -26,6 +29,12 @@ public class FinalCutSceneManager : MonoBehaviour
         cameraManager.ada3Camera.Priority = 0;
         cameraManager.ada4Camera.Priority = 0;
         cameraManager.ada5Camera.Priority = 0;
+
+        ResetCamera(cameraManager.ada1Camera);
+        ResetCamera(cameraManager.ada2Camera);
+        ResetCamera(cameraManager.ada3Camera);
+        ResetCamera(cameraManager.ada4Camera);
+        ResetCamera(cameraManager.ada5Camera);
     }
 
     private void Update()
@@ -34,43 +43,54 @@ public class FinalCutSceneManager : MonoBehaviour
     }
     void PlayFinalCutscene()
     {
-        
+        PlayerStateData.Singleton.currentMainState = PlayerStateData.PlayerMainState.PauseMenuState;
+
         MoveCamera(cameraManager.ada5Camera);
 
-        if(HasCameraMoved(cameraManager.ada5Camera));
-         MoveCamera(cameraManager.ada4Camera); //Kameranýn prio su sürekli yüksek kalýyor.
-        //Fonksiyonun burdan sonrasnýn çalýþtýðýný görmedim.
-        if(HasCameraMoved(cameraManager.ada4Camera))
-        MoveCamera(cameraManager.ada3Camera);
+        if (HasCameraMoved(cameraManager.ada5Camera))
+        MoveCamera(cameraManager.ada4Camera); 
 
-        if(HasCameraMoved(cameraManager.ada3Camera))
-        MoveCamera(cameraManager.ada2Camera);
+        if (HasCameraMoved(cameraManager.ada4Camera))
+            MoveCamera(cameraManager.ada3Camera);
 
-        if(HasCameraMoved(cameraManager.ada2Camera))
-        MoveCamera(cameraManager.ada1Camera);
+        if (HasCameraMoved(cameraManager.ada3Camera))
+            MoveCamera(cameraManager.ada2Camera);
+
+        if (HasCameraMoved(cameraManager.ada2Camera))
+            MoveCamera(cameraManager.ada1Camera);
+
+        if (HasCameraMoved(cameraManager.ada1Camera))
+            cameraManager.SwitchCameras(cameraManager.playerCamera);
+
+        StartCoroutine(WaitFor(2));
+        PlayerStateData.Singleton.currentMainState = PlayerStateData.PlayerMainState.NormalState;
 
         return;
     }
 
+    IEnumerator WaitFor(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+    }
+
     void MoveCamera(CinemachineVirtualCamera cameraToMove)
     {
-        if(!HasCameraMoved(cameraToMove))
-        {
             cameraManager.SwitchCameras(cameraToMove); 
             cameraToMove.GetCinemachineComponent<CinemachineTrackedDolly>().m_PathPosition += cameraMoveSpeed*Time.deltaTime; 
-        }
-        else cameraToMove.Priority = 0;
     }
     bool HasCameraMoved(CinemachineVirtualCamera cameraToMove)
     {
-        if (cameraToMove.GetCinemachineComponent<CinemachineTrackedDolly>().m_PathPosition >= 1)
+        if (cameraToMove.GetCinemachineComponent<CinemachineTrackedDolly>().m_PathPosition >= cameraMoveLimit)
         {
-            cameraToMove.GetCinemachineComponent<CinemachineTrackedDolly>().m_PathPosition = 0;
             return true;
         }
         else
         {
             return false;
         }   
+    }
+    void ResetCamera(CinemachineVirtualCamera cameraToReset)
+    {
+        cameraToReset.GetCinemachineComponent<CinemachineTrackedDolly>().m_PathPosition = 0;
     }
 }
