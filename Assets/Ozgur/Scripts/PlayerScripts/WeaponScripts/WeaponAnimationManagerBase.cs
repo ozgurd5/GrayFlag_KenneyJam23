b4972189a7ff6d4039ec0f10be35a2ddcf50d7ee
Özgür Dalbeler <ozgurdalbeler@gmail.com>
2023-10-23 09:19:05 +0300

    changes in description
    dotween errors fixed
    attack animation bug fixed
    better running/walking/attack logic structure
    cooldown slider tween is linear

--- Assets/Ozgur/Scripts/PlayerScripts/WeaponScripts/WeaponAnimationManagerBase.cs
+++ Assets/Ozgur/Scripts/PlayerScripts/WeaponScripts/WeaponAnimationManagerBase.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using DG.Tweening;
 using UnityEngine;
@@ -32,10 +33,10 @@ public class WeaponAnimationManagerBase : MonoBehaviour
     private bool isHidingAnimationPlaying;
 
     //Attacking
-    private Tweener attackRotationTween;
+    private Tweener attackAnimationTween;
     public bool isAttackAnimationPlaying; //PlayerHookController.cs needs access
+    private bool isAttackAnimationReturning; //Returning means weapon getting back to attackRotationXBack. Detailed explanation is in PlayAttackAnimation()
     private float attackRotationXBack;
-
 
     /// <summary>
     /// Call this method in the Awake method of the child scripts
@@ -47,7 +48,9 @@ public class WeaponAnimationManagerBase : MonoBehaviour
         exposeWeaponAnimation = PlayExposeWeaponAnimation();
 
         //Walking values are the default values
-        DisableRunningMode();
+        attackRotationXBack = wav.walkingModeRotationX;
+        movingAnimationPositionZ = wav.walkingModeMovingAnimationPositionZ;
+        movingAnimationPositionZBack = wav.walkingModeMovingAnimationPositionZBack;
     }
 
     /// <summary>
@@ -104,12 +107,12 @@ public class WeaponAnimationManagerBase : MonoBehaviour
     {
         isMovingAnimationPlaying = true;
 
-        movingTweenZ = weaponTransform.DOLocalMoveZ(movingAnimationPositionZ, movingAnimationHalfDuration);
         movingTweenY = weaponTransform.DOLocalMoveY(wav.movingAnimationPositionY, movingAnimationHalfDuration);
+        movingTweenZ = weaponTransform.DOLocalMoveZ(movingAnimationPositionZ, movingAnimationHalfDuration);
         yield return new WaitForSeconds(movingAnimationHalfDuration);
 
-        movingTweenZ = weaponTransform.DOLocalMoveY(wav.movingAnimationPositionYBack, movingAnimationHalfDuration);
-        movingTweenY = weaponTransform.DOLocalMoveZ(movingAnimationPositionZBack, movingAnimationHalfDuration);
+        movingTweenY = weaponTransform.DOLocalMoveY(wav.movingAnimationPositionYBack, movingAnimationHalfDuration);
+        movingTweenZ = weaponTransform.DOLocalMoveZ(movingAnimationPositionZBack, movingAnimationHalfDuration);
         yield return new WaitForSeconds(movingAnimationHalfDuration);
 
         isMovingAnimationPlaying = false;
@@ -123,8 +126,12 @@ public class WeaponAnimationManagerBase : MonoBehaviour
         movingTweenZ.Kill();
         movingTweenY.Kill();
 
-        if (PlayerStateData.Singleton.isIdle) weaponTransform.DOLocalMoveY(wav.movingAnimationPositionYBack, 0.1f);
-        weaponTransform.DOLocalMoveZ(movingAnimationPositionZBack, 0.1f);
+        //If character stops, weapon must be in it's default position
+        if (PlayerStateData.Singleton.isIdle)
+        {
+            weaponTransform.DOLocalMoveY(wav.movingAnimationPositionYBack, 0.1f);
+            weaponTransform.DOLocalMoveZ(movingAnimationPositionZBack, 0.1f);
+        }
     }
 
     #endregion
@@ -133,30 +140,50 @@ public class WeaponAnimationManagerBase : MonoBehaviour
 
     private void EnableRunningMode()
     {
+        isRunningModeActive = true;
+
         attackRotationXBack = wav.runningModeRotationX;
         movingAnimationPositionZ = wav.runningModeMovingAnimationPositionZ;
         movingAnimationPositionZBack = wav.runningModeMovingAnimationPositionZBack;
 
         StopMovingAnimation();
-
         runningModeRotationTween.Kill();
-        runningModeRotationTween = weaponTransform.DOLocalRotate(new Vector3(wav.runningModeRotationX, wav.defaultRotationY, 0f), 0.1f);
 
-        isRunningModeActive = true;
+        //Both attack animation and running mode changing animation controls X rotation. Their conflict causes strange things and we must prevent it.
+        //Attack animation has higher priority, so when it's playing, this must not.
+        if (!isAttackAnimationPlaying)
+            runningModeRotationTween = weaponTransform.DOLocalRotate(new Vector3(wav.runningModeRotationX, wav.defaultRotationY, 0f), wav.runningModeSwitchDuration);
+
+        //Detailed explanation about why we are doing this is down bellow in PlayAttackAnimation()
+        else if (isAttackAnimationReturning)
+        {
+            attackAnimationTween.Kill();
+            attackAnimationTween = weaponTransform.DOLocalRotate(new Vector3(attackRotationXBack, wav.defaultRotationY, 0f), wav.attackAnimationHalfDuration);
+        }
     }
 
     private void DisableRunningMode()
     {
+        isRunningModeActive = false;
+
         attackRotationXBack = wav.walkingModeRotationX;
         movingAnimationPositionZ = wav.walkingModeMovingAnimationPositionZ;
         movingAnimationPositionZBack = wav.walkingModeMovingAnimationPositionZBack;
 
         StopMovingAnimation();
-
         runningModeRotationTween.Kill();
-        runningModeRotationTween = weaponTransform.DOLocalRotate(new Vector3(wav.walkingModeRotationX, wav.defaultRotationY, 0f), 0.1f);
 
-        isRunningModeActive = false;
+        //Both attack animation and running mode changing animation controls X rotation. Their conflict causes strange things and we must prevent it.
+        //Attack animation has higher priority, so when it's playing, this must not.
+        if (!isAttackAnimationPlaying)
+            runningModeRotationTween = weaponTransform.DOLocalRotate(new Vector3(wav.walkingModeRotationX, wav.defaultRotationY, 0f), wav.runningModeSwitchDuration);
+
+        //Detailed explanation about why we are doing this is down bellow in PlayAttackAnimation()
+        else if (isAttackAnimationReturning)
+        {
+            attackAnimationTween.Kill();
+            attackAnimationTween = weaponTransform.DOLocalRotate(new Vector3(attackRotationXBack, wav.defaultRotationY, 0f), wav.attackAnimationHalfDuration);
+        }
     }
 
     #endregion
@@ -219,15 +246,33 @@ public class WeaponAnimationManagerBase : MonoBehaviour
 
     protected IEnumerator PlayAttackAnimation()
     {
+        runningModeRotationTween.Kill();
+
         isAttackAnimationPlaying = true;
 
-        attackRotationTween = weaponTransform.DOLocalRotate(new Vector3(wav.attackRotationX, wav.defaultRotationY, 0f), wav.attackAnimationHalfDuration);
-        DOTween.To(() => cooldownSlider.value, value => cooldownSlider.value = value, 0, wav.attackAnimationHalfDuration);
+        //cooldownSlider tween is linear because default tween ease is outQuad which is getting slower while it's getting closer to the destination.
+        //This is confusing for the player and it's better for slider to move at constant speed, so the player can **feel** when the slider is full..
+        //..and when he/she can attack again. OutQuad makes the *feeling* harder because it's getting slower when it's closer.
+
+        attackAnimationTween = weaponTransform.DOLocalRotate(new Vector3(wav.attackRotationX, wav.defaultRotationY, 0f), wav.attackAnimationHalfDuration);
+        DOTween.To(() => cooldownSlider.value, value => cooldownSlider.value = value, 0, wav.attackAnimationHalfDuration).SetEase(Ease.Linear);
         yield return new WaitForSeconds(wav.attackAnimationHalfDuration);
 
-        attackRotationTween = weaponTransform.DOLocalRotate(new Vector3(attackRotationXBack, wav.defaultRotationY, 0f), wav.attackAnimationHalfDuration);
-        DOTween.To(() => cooldownSlider.value, value => cooldownSlider.value = value, 1, wav.attackAnimationHalfDuration);
+        //We need to keep track of returning part of the animation, because when player change it's running/walking state while returning part is playing,..
+        //..it goes to the previous state's attackRotationXBack value.
+
+        //For example: While returning part is playing, player is in running mode ant the DOLocalRotate tween down bellow started with runningModeRotationX..
+        //..and it x value goes there. But the player switches from running to walking. We are now in walking state and x value must reach walkingModeRotationX..
+        //..but it's still going runningModeRotationX
+
+        //So in order to do that we must keep track of when returning animation plays. If it's playing and player switches state, we must stop the active..
+        //..tween and create another one with the correct attackRotationXBack value which is either runningModeRotationX or walkingModeRotationX.
+
+        isAttackAnimationReturning = true;
+        attackAnimationTween = weaponTransform.DOLocalRotate(new Vector3(attackRotationXBack, wav.defaultRotationY, 0f), wav.attackAnimationHalfDuration);
+        DOTween.To(() => cooldownSlider.value, value => cooldownSlider.value = value, 1, wav.attackAnimationHalfDuration).SetEase(Ease.Linear);
         yield return new WaitForSeconds(wav.attackAnimationHalfDuration);
+        isAttackAnimationReturning = false;
 
         isAttackAnimationPlaying = false;
     }
